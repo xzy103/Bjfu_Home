54447b419e6dee220197f1403df7783c768dec1a
xzy103 <1342638994@qq.com>
2019-04-26 18:23:28 +0800

    upgrade

---
+++ BjfuHomeRun.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+# author: xzy103
+# ver: 1.0(190421)
+
+from BjfuHome import BjfuWifi, BjfuWeb, BjfuScore, BjfuMail, BjfuLib, BjfuFtp, BjfuEm, BjfuCourse
+from prettytable import PrettyTable
+import time
+import sys
+import os
+import ctypes
+
+
+# 命令行输出文字颜色
+class PyColor(object):
+    def __init__(self):
+        self.STD_INPUT_HANDLE = -10
+        self.STD_OUTPUT_HANDLE = -11
+        self.STD_ERROR_HANDLE = -12
+
+        # Windows CMD命令行 字体颜色定义 text colors
+        self.FOREGROUND_GREEN = 0x0a  # green.
+        self.FOREGROUND_RED = 0x0c  # red.
+        self.FOREGROUND_BLUE = 0x09  # blue.
+        self.FOREGROUND_YELLOW = 0x0e  # yellow.
+
+        # get handle
+        self.std_out_handle = ctypes.windll.kernel32.GetStdHandle(self.STD_OUTPUT_HANDLE)
+
+    def set_cmd_text_color(self, color):
+        Bool = ctypes.windll.kernel32.SetConsoleTextAttribute(self.std_out_handle, color)
+        return Bool
+
+    # reset white
+    def resetColor(self):
+        self.set_cmd_text_color(self.FOREGROUND_RED | self.FOREGROUND_GREEN | self.FOREGROUND_BLUE)
+
+    # green
+    def printGreen(self, mess):
+        self.set_cmd_text_color(self.FOREGROUND_GREEN)
+        sys.stdout.write(mess+'\n')
+        self.resetColor()
+
+    # red
+    def printRed(self, mess):
+        self.set_cmd_text_color(self.FOREGROUND_RED)
+        sys.stdout.write(mess+'\n')
+        self.resetColor()
+
+    # yellow
+    def printYellow(self, mess):
+        self.set_cmd_text_color(self.FOREGROUND_YELLOW)
+        sys.stdout.write(mess+'\n')
+        self.resetColor()
+
+
+lst = [
+    ['1', '查询课表'],
+    ['2', '获取经管院通知'],
+    ['3', '北林常用FTP'],
+    ['4', '北林图书馆检索'],
+    ['5', '126/163邮箱登录'],
+    ['6', '查询最新成绩'],
+    ['7', '北林常用网站'],
+    ['8', '一键登录校园网'],
+    ['0', '退出程序']
+]
+pc = PyColor()
+table = PrettyTable(['序号', '功能'])
+for it in lst:
+    table.add_row(it)
+table.align = 'l'
+
+while True:
+    os.system('cls')
+    pc.printGreen('>>>>>>北林全家桶 | 合集版')
+    print(table)
+    mode = input('请输入对应序号：')
+    os.system('cls')
+
+    if mode == '0':
+        pc.printYellow('>>>>>即将退出程序')
+        time.sleep(1)
+        sys.exit()
+    elif mode == '1':
+        pc.printGreen('{:-^80}'.format(lst[0][-1]))
+        time.sleep(1)
+        BjfuCourse.main()
+    elif mode == '2':
+        pc.printGreen('{:-^80}'.format(lst[1][-1]))
+        time.sleep(1)
+        BjfuEm.main()
+    elif mode == '3':
+        pc.printGreen('{:-^80}'.format(lst[2][-1]))
+        time.sleep(1)
+        BjfuFtp.main()
+    elif mode == '4':
+        pc.printGreen('{:-^80}'.format(lst[3][-1]))
+        time.sleep(1)
+        BjfuLib.main()
+    elif mode == '5':
+        pc.printGreen('{:-^80}'.format(lst[4][-1]))
+        time.sleep(1)
+        BjfuMail.main()
+    elif mode == '6':
+        pc.printGreen('{:-^80}'.format(lst[5][-1]))
+        time.sleep(1)
+        BjfuScore.main()
+    elif mode == '7':
+        pc.printGreen('{:-^80}'.format(lst[6][-1]))
+        time.sleep(1)
+        BjfuWeb.main()
+    elif mode == '8':
+        pc.printGreen('{:-^80}'.format(lst[7][-1]))
+        time.sleep(1)
+        BjfuWifi.main()
+    else:
+        pc.printRed('非法输入！')
+        print('>>>>>请重新输入...')
+        time.sleep(2)
--- README.md
+++ README.md
@@ -8,13 +8,24 @@
 >4.北林常用ftp下载  
 >5.一键登录校园网  
 >6.教务系统查成绩  
->7.教务系统查课程表  
+>7.教务系统查课表  
 >8.获取经管院最新通知  
 
-## 文件夹说明
-`北林全家桶合集版`包含了一个`Run_BjfuHome.py`文件，其他文件是依赖文件；  
-`北林全家桶八大件`包含了各单独功能的python文件和ico图标文件；  
-`py2exe`文件夹下有一个`py2exe.py`的文件，其作用是将`.py文件`打包成`.exe文件`，依赖的python库是[pyinstaller](https://github.com/pyinstaller/pyinstaller)  
+## 结构说明
+`BjfuHomeRun.py`是打包版的主文件，集成了全家桶八大功能，依赖`BjfuHomeRun文件夹`下的各个子功能.py文件;  
+`BjfuHome文件夹`中的`icos文件夹`存放的是各子功能对应的图标文件，用于打包;  
+`BjfuHome文件夹`中的`py2exe文件夹`存放的是用于将`.py文件`打包成`.exe文件`的python脚本;  
+也可以使用[pyinstaller](https://github.com/pyinstaller/pyinstaller)库自行打包;  
+>打包方法  
+>在命令行下cd到.py和.ico文件所在的文件夹  
+>使用命令`pyinstaller -i xxx.ico -F xxx.py`  
+>如果没有cd，使用绝对路径即可，也可以不使用图标文件直接打包  
+
+## 依赖的第三方库
+[requests](https://github.com/pyinstaller/pyinstaller)  
+[pywifi](https://github.com/awkman/pywifi)  
+[beautifulsoup4](https://pypi.org/project/beautifulsoup4/)  
+[prettytable](https://pypi.org/project/PrettyTable/)  
 
 ## 关于作者
 BJFU非计算机专业的一只野生猿，欢迎与我交流Python。(๑¯∀¯๑)  
